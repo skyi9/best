dc660456d63ad69b070439e4cbe477d0eb0320d2
skyi9 <88197965+skyi9@users.noreply.github.com>
2023-08-26 11:16:28 +0530

    post: delete and like-dislike implemented

--- Backend/controllers/post.js
+++ Backend/controllers/post.js
@@ -1,7 +1,6 @@
 const Post = require("../models/Post");
 const User = require("../models/User");
 
-
 exports.createPost = async (req, res) => {
     try {
         const newPostData = {
@@ -25,5 +24,77 @@ exports.createPost = async (req, res) => {
             success: false,
             message: error.message
         })
+    }
+}
+
+exports.deletePost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) {
+            return res.status(404).json({
+                success: false,
+                message: "post not found",
+            });
+        }
+        if (post.owner.toString() !== req.user._id.toString()) {
+            return res.status(400).json({
+                success: false,
+                message: "unAuthorized"
+            })
+        }
+        await post.deleteOne();
+        const user = await User.findById(req.user._id);
+        const index = user.posts.indexOf(req.params.id);
+        user.posts.splice(index, 1);
+        await user.save();
+        res.status(200).json({
+            success: true,
+            message: "post deleted"
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+exports.likeAndUnlikePost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({
+                success: false,
+                message: "post not found",
+            });
+        }
+
+        if (post.likes.includes(req.user._id)) {
+            const index = post.likes.indexOf(req.user._id);
+
+            post.likes.splice(index, 1);
+
+            await post.save();
+
+            return res.status(200).json({
+                success: true,
+                message: "post Unliked",
+            });
+        } else {
+            post.likes.push(req.user._id);
+
+            await post.save();
+
+            return res.status(200).json({
+                success: true,
+                message: "post Liked",
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
     }
 }
--- Backend/routes/post.js
+++ Backend/routes/post.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const { isAuthenticated } = require("../middlewares/auth");
-const { createPost } = require("../controllers/post");
+const { createPost, likeAndUnlikePost, deletePost } = require("../controllers/post");
 const router = express.Router();
 
 router.route("/post/upload").post(isAuthenticated, createPost);
+router.route("/post/:id")
+    .get(isAuthenticated, likeAndUnlikePost)
+    .delete(isAuthenticated, deletePost)
 
 module.exports = router;
