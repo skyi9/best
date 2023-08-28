b57588fcd9c7f1542e1a0ac898641bf7858b5d96
skyi9 <88197965+skyi9@users.noreply.github.com>
2023-08-28 23:29:04 +0530

    get all users api done

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -294,3 +294,19 @@ exports.getUserProfile = async (req, res) => {
         });
     }
 };
+
+exports.getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+
+        res.status(200).json({
+            success: true,
+            users
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+}
