982999aa266b72df23ab8d764036622c91a4fb18
skyi9 <88197965+skyi9@users.noreply.github.com>
2023-08-28 23:23:11 +0530

    delete account , get profile etc.

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,3 +1,4 @@
+const Post = require('../models/Post');
 const User = require('../models/User');
 
 exports.register = async (req, res) => {
@@ -185,3 +186,111 @@ exports.updateProfile = async (req, res) => {
         })
     }
 }
+
+exports.deleteMyProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id);
+        const posts = user.posts;
+        const followers = user.followers;
+        const following = user.following;
+        const userId = user._id;
+
+        await user.deleteOne();
+        // Logout user after deleting profile
+        res.cookie("token", null, {
+            expires: new Date(Date.now()),
+            httpOnly: true,
+        });
+        // Delete all posts of the user
+        for (let i = 0; i < posts.length; i++) {
+            const post = await Post.findById(posts[i]);
+            await post.remove();
+        }
+        // Removing User from Followers Following
+        for (let i = 0; i < followers.length; i++) {
+            const follower = await User.findById(followers[i]);
+            const index = follower.following.indexOf(userId);
+            follower.following.splice(index, 1);
+            await follower.save();
+        }
+        // Removing User from Following's Followers
+        for (let i = 0; i < following.length; i++) {
+            const follows = await User.findById(following[i]);
+            const index = follows.followers.indexOf(userId);
+            follows.followers.splice(index, 1);
+            await follows.save();
+        }
+        // removing all comments of the user from all posts
+        const allPosts = await Post.find();
+        for (let i = 0; i < allPosts.length; i++) {
+            const post = await Post.findById(allPosts[i]._id);
+
+            for (let j = 0; j < post.comments.length; j++) {
+                if (post.comments[j].user === userId) {
+                    post.comments.splice(j, 1);
+                }
+            }
+            await post.save();
+        }
+        // removing all likes of the user from all posts
+        for (let i = 0; i < allPosts.length; i++) {
+            const post = await Post.findById(allPosts[i]._id);
+            for (let j = 0; j < post.likes.length; j++) {
+                if (post.likes[j] === userId) {
+                    post.likes.splice(j, 1);
+                }
+            }
+            await post.save();
+        }
+        res.status(200).json({
+            success: true,
+            message: "Profile Deleted",
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
+exports.myProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id).populate(
+            "posts followers following"
+        );
+
+        res.status(200).json({
+            success: true,
+            user,
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
+exports.getUserProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id).populate(
+            "posts followers following"
+        );
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found",
+            });
+        }
+        res.status(200).json({
+            success: true,
+            user,
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+};
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, login, followUser, logout, updatePassword, updateProfile } = require('../controllers/user');
+const { register, login, followUser, logout, updatePassword, updateProfile, deleteMyProfile, myProfile, getUserProfile } = require('../controllers/user');
 const { isAuthenticated } = require("../middlewares/auth");
 const router = express.Router();
 
@@ -10,6 +10,9 @@ router.route("/logout").get(logout);
 router.route("/follow/:id").get(isAuthenticated, followUser);
 router.route("/update/password").put(isAuthenticated, updatePassword);
 router.route("/update/profile").put(isAuthenticated, updateProfile);
+router.route("/delete/me").delete(isAuthenticated, deleteMyProfile);
+router.route("/me").get(isAuthenticated, myProfile);
+router.route("/user/:id").get(isAuthenticated, getUserProfile);
 
 
 module.exports = router;
